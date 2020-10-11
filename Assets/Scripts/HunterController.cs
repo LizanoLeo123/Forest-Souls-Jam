ea7fd24bcc1b25fb131ade23690e837a14e53c90
Ricardo Leon <ricardo.cr023@gmail.com>
2020-10-10 22:18:58 -0600

    Changed player sprites. Added Hunter animation, along with its arm.

---
+++ Assets/Scripts/HunterController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HunterController : MonoBehaviour
+{
+    private GameObject hunter;
+    private GameObject hunterArm;
+    
+    void Awake()
+    {
+        hunter = GameObject.Find("Hunter");
+        hunterArm = GameObject.Find("HunterArm");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        hunterArm.transform.position = hunter.transform.position + new Vector3(0,0,1);
+    }
+}
