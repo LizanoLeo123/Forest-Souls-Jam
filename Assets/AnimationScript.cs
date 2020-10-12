f344bcefc5c884a4007f42d1f4596f061c5f9af0
Andrey Rojas <andrey20ag95@gmail.com>
2020-10-11 18:59:29 -0600

    Menu principal completo.

---
+++ Assets/AnimationScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AnimationScript : MonoBehaviour
+{
+	public Sprite[] animatedImages;
+	public Image animateImageObj;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        animateImageObj.sprite = animatedImages[(int)(Time.time*4)%animatedImages.Length];
+    }
+}
